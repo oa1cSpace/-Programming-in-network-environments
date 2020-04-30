f9a35ddbacc87ae818bee09a969091dc9cbaf219
oa1cspace <oa1cspace@gmail.com>
2020-04-30 12:39:47 +0300

    Lab 2 task3 Watch

---
+++ Lab2/task2-watch/main.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+  window.setInterval(
+    function(){
+      var d = new Date();
+      document.getElementById('watch').innerHTML = d.toLocaleTimeString();
+    }, 1000
+  );
+}
